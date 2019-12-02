6c6e3f7f8314c7dde5eb8daab9dc2891085a947f
reyman-escolar <52724397+reyman-escolar@users.noreply.github.com>
2019-12-02 12:42:30 -0500

    Boton mas grande

--- Inventario/Form1.Designer.cs
+++ Inventario/Form1.Designer.cs
@@ -33,12 +33,12 @@
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(118, 97);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(118, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(423, 341);
+            this.button1.Size = new System.Drawing.Size(423, 438);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Boton Reyman";
+            this.button1.Text = "Boton ";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
